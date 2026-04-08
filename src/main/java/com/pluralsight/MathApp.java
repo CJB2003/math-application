08f1d2e19e128002bd587a3785fb5b83dec455d2
chrisbunny <christopherjbunny@gmail.com>
2026-04-08 15:22:22 -0500

    Added areaOfCircle

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+
+        //Step #1
+        double bobSalary = 65891.29;
+        double garySalary = 78992.68;
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("The highest salary is " + highestSalary + "\n");
+
+        double carPrice = 30210.88;
+        double truckPrice = 56720.99;
+
+        System.out.println("The lowest vehicle price is " + Math.min(carPrice, truckPrice) + "\n");
+
+        double radiusOfCircle = 7.25;
+        double areaOfCircle = Math.PI * Math.sqrt(radiusOfCircle);
+
+        System.out.println(areaOfCircle);
+
+    }
 }
